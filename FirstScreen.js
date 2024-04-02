ae3bac2e60fdd0744c9915fc75c32ac2e9e1aacd
Abdul-Mateen11 <145853946+Abdul-Mateen11@users.noreply.github.com>
2024-04-03 04:37:51 +0500

    Add files via upload

---
+++ FirstScreen.js
@@ -0,0 +1,123 @@
+import { StatusBar } from 'expo-status-bar';
+import { Alert, StyleSheet, Text, Touchable, TouchableOpacity, View , Image , TextInput } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+export default function Myscreen() {
+
+  const Stack = createNativeStackNavigator();
+  //const [text, onChangeText] = React.useState('Username');
+  const [number, onChangeNumber] = React.useState('');
+  const youpressedme = () => {
+  Alert.alert('I Love you Too ')    
+  }
+  const onPress = () => {
+    Alert.alert('Logged IN')    
+    }
+  return (
+
+    
+    <View style={styles.container}>
+       <Text>Do You Love Me?</Text>
+      {/*<TouchableOpacity
+      onPress={youpressedme}
+      style={{backgroundColor:'red'}}>
+
+        <Text>YES</Text>
+        <Text>NO</Text>
+      </TouchableOpacity>
+      <StatusBar style="auto" />
+
+      <Image
+        style={{width:100,height:100}}
+        source={require('./download.jpeg')}
+      />
+      <Image
+        style={{width:100,height:100}}
+        source={{
+          uri: 'https://staticg.sportskeeda.com/editor/2022/01/0d8a5-16422638539293-1920.jpg',
+        }}
+      />
+
+<TextInput
+        style={styles.input}
+        //onChangeText={onChangeText}
+        //value={text}
+      />
+      <TextInput
+        style={styles.input}
+        //onChangeText={onChangeNumber}
+        // value={number}
+        placeholder="useless placeholder"
+        // keyboardType="numeric"
+      /> */}
+      {/* <View style={styles.top}>
+      <Image
+        style={{width:100,height:100}}
+        source={{
+          uri: 'https://staticg.sportskeeda.com/editor/2022/01/0d8a5-16422638539293-1920.jpg',
+        }}
+      />
+      </View>
+      <View style={styles.center}>
+      <TextInput
+        style={styles.input}
+        //onChangeText={onChangeText}
+        placeholder="Username"
+        //value={text}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="Password"
+        keyboardType="numeric"
+      />
+      </View>  
+      <View style={styles.bottom}>
+      <TouchableOpacity style={styles.button} onPress={onPress}>
+        <Text>Press Here</Text>
+      </TouchableOpacity>
+      </View>*/}
+    </View> 
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    //backgroundColor: '#fff',
+    //alignItems: 'center',
+    //justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  top: {
+    flex: 25,
+    //backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  center: {
+    flex: 50,
+    //backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bottom: {
+    flex: 25,
+    backgroundColor: 'blue',
+    //alignItems: 'center',
+    //justifyContent: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    //backgroundColor: '#DDDDDD',
+    padding: 10,
+  },
+});
